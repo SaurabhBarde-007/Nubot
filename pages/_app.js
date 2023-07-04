927a10df401969472781f591f9ff92d4e1053ef1
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-05 01:02:22 +0530

    initial loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,31 @@
 import '@/styles/globals.css'
+import { useEffect, useState } from "react";
+import LoadingScreen from "./loader"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  
+  const delay = setTimeout(() => {
+    setLoading(false);
+    clearTimeout(delay);
+  }, 6000);
+
+  
+  return () => clearTimeout(delay);
+}, []);
+
+return (
+  <>
+    {loading ? (
+      <LoadingScreen />
+    ) : (
+      <>
+        <Component {...pageProps} />
+      </>
+    )}
+  </>
+  )
+
 }
