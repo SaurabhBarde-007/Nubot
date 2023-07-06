ea48f5f3f147a9309928b713247532b01fbd6f01
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-06 18:34:04 +0530

    serach functionality created

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import { AiOutlineArrowRight } from "react-icons/ai";
+import React, { useState } from 'react'
+import { AiFillCaretDown } from "react-icons/ai";
 
 const Button = (props) => {
     const{src,text} = props
+
+    const[active,setActive] = useState(false);
+
   return (
-    <button className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' >
-      <img src={src} className='w-[30px] h-[30px] felx self-center'/>
-      <span className="felx self-center whitespace-normal text-start text-white w-[300px]">
+    <button className='w-[100px] sm:w-[150px]  h-[50px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-2 ' onClick={()=>{
+      setActive(!active);
+    }}>
+      <img src={src} className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
+      <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
         {text}
       </span >
-      <AiOutlineArrowRight className='w-[25px] h-[25px] felx self-center'/>
+      <AiFillCaretDown className='w-[25px] h-[25px] felx self-center'/>
+      {active && 
+        <div className='absolute top-[54px] z-40 left-[6px] bg-[#aaa] rounded-lg w-[98px] sm:w-[148px] h-auto p-[5px] flex flex-col justify-center content-center gap-1'>
+          
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+              <img src="./eth.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
+              <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
+                Ethereum
+              </span >
+            </button>
+
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+              <img src="./sol.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
+              <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
+                Solana
+              </span >
+            </button>
+
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+              <img src="./pol.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
+              <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
+                Polygon
+              </span >
+            </button>
+
+        </div>
+        }
     </button>
   )
 }
