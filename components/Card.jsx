5219de7f086505b5fd05c9d917f309e71e91815d
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-07 10:47:03 +0530

    search functionality

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Card = (props) => {
-    const{src,text} = props
+    const{src,text,func} = props
   return (
-    <button className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' >
+    <button className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' onClick={()=>{
+      func();
+    }}>
       <img src={src} className='w-[30px] h-[30px] felx self-center'/>
       <span className="felx self-center whitespace-normal text-start text-white w-[300px]">
         {text}
--- pages/query/index.js
+++ pages/query/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Card from "@/components/Card"
 import Button from "@/components/Button"
+import { useState } from 'react'
+
 
 const Index = () => {
+     const[search,setSearch] = useState("");
+
   return (
     <div className='w-[100vw] h-[88.5vh] bg-white mt-[76px]'>
         <div className='w-[100vw] h-[200px] flex justify-center content-center relative' >
@@ -11,7 +15,7 @@ const Index = () => {
                     <Button src="./eth.png" text="Ethereum"/>
                     <div className='w-[2px] h-[50px] rounded-lg bg-[#aaa] mx-2 flex self-center'></div>
                     <div className='w-[70vw] sm:w-[55vw] md:w-[45vw] lg:w-[40vw] xl:w-[28vw] h-[50px] rounded-lg bg-black flex self-center'>
-                        <input type='text' placeholder='Mention @ to tag NFT collections or Marketplaces' className='w-[100%] h-[100%] rounded-sm p-2 text-[#000] focus:outline-none' />
+                        <input id="search" type='text' placeholder='Mention @ to tag NFT collections or Marketplaces' className='w-[100%] h-[100%] rounded-sm p-2 text-[#000] focus:outline-none' />
                     </div>
                 </div>
                 
@@ -27,9 +31,21 @@ const Index = () => {
                 <span className='text-black flex self-start '>Try These</span>
             </div>
             
-            <Card src="./eth.png" text="top 5 collections on Blur marketplace in last 7 days"/>
-            <Card src="./sol.png" text="top 5 top 10 holders of DeGods collection"/>
-            <Card src="./pol.png" text="top 10 sales of y00ts collection"/>
+            <Card src="./eth.png" text="top 5 collections on Blur marketplace in last 7 days" func={()=>{
+                document.getElementById("search").value="";
+                const newValue = "top 5 collections on Blur marketplace in last 7 days";
+                document.getElementById("search").value=newValue;
+            }} />
+            <Card src="./sol.png" text="top 5 top 10 holders of DeGods collection" func={()=>{
+                document.getElementById("search").value="";
+                const newValue = "top 5 top 10 holders of DeGods collection";
+                document.getElementById("search").value=newValue;
+            }}/>
+            <Card src="./pol.png" text="top 10 sales of y00ts collection" func={()=>{
+                document.getElementById("search").value="";
+                const newValue = "top 10 sales of y00ts collection";
+                document.getElementById("search").value=newValue;
+            }}/>
         </div>
     </div>
   )
