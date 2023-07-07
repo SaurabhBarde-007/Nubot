f62e23c7d803e09c24711a329ef972299812a2e3
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-07 10:02:31 +0530

    search functionality

--- components/Button.jsx
+++ components/Button.jsx
@@ -6,33 +6,45 @@ const Button = (props) => {
 
     const[active,setActive] = useState(false);
 
+    const[srcUpdate,setSrcUpdate] = useState(src);
+    const[textUpdate,setTextUpdate] = useState(text);
+
   return (
     <button className='w-[100px] sm:w-[150px]  h-[50px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-2 ' onClick={()=>{
       setActive(!active);
     }}>
-      <img src={src} className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
+      <img src={srcUpdate} className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
       <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
-        {text}
+        {textUpdate}
       </span >
       <AiFillCaretDown className='w-[25px] h-[25px] felx self-center'/>
       {active && 
         <div className='absolute top-[54px] z-40 left-[6px] bg-[#aaa] rounded-lg w-[98px] sm:w-[148px] h-auto p-[5px] flex flex-col justify-center content-center gap-1'>
           
-            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' onClick={()=>{
+              setSrcUpdate("./eth.png");
+              setTextUpdate("Ethereum");
+            }} >
               <img src="./eth.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
               <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
                 Ethereum
               </span >
             </button>
 
-            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' onClick={()=>{
+              setSrcUpdate("./sol.png");
+              setTextUpdate("Solana");
+            }} >
               <img src="./sol.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
               <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
                 Solana
               </span >
             </button>
 
-            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' >
+            <button className='p-[10px] w-[90px] sm:w-[140px]  h-[50px] flex justify-center sm:justify-start content-center self-center rounded-lg gap-2 ' onClick={()=>{
+              setSrcUpdate("./pol.png");
+              setTextUpdate("Polygon");
+            }} >
               <img src="./pol.png" className='w-[30px] h-[30px] sm:w-[25px] sm:h-[25px] felx self-center'/>
               <span className=" self-center whitespace-normal text-start text-white w-auto hidden sm:flex">
                 Polygon
