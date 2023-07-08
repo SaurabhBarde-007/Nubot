307bc75ee88a0bc6e4f8324d819178dffa3a9187
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-08 15:25:09 +0530

    framer motion effects added

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { motion} from "framer-motion";
 
 const Card = (props) => {
     const{src,text,func} = props
   return (
-    <button className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' onClick={()=>{
+    <motion.button whileHover={{scale:1.05}} className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' onClick={()=>{
       func();
     }}>
       <img src={src} className='w-[30px] h-[30px] felx self-center'/>
@@ -12,7 +13,7 @@ const Card = (props) => {
         {text}
       </span >
       <AiOutlineArrowRight className='w-[25px] h-[25px] felx self-center'/>
-    </button>
+    </motion.button>
   )
 }
 
