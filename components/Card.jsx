1b562acd39e12f161dc34a17e46c7cd218cc30a5
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-08 15:34:43 +0530

    minor changes

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,7 +5,7 @@ import { motion} from "framer-motion";
 const Card = (props) => {
     const{src,text,func} = props
   return (
-    <motion.button whileHover={{scale:1.05}} className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' onClick={()=>{
+    <motion.button transition={{duration:0.2}} whileHover={{scale:1.05}} className='w-[300px] sm:w-[450px] h-[90px] bg-[#000] flex justify-center content-center self-center rounded-lg gap-4 p-[20px]' onClick={()=>{
       func();
     }}>
       <img src={src} className='w-[30px] h-[30px] felx self-center'/>
